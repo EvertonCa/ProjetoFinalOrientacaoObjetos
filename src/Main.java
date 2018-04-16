2ca687393fb344fa9ad270bccbcdd08875e42051
EvertonCa <eve.023@hotmail.com>
2018-04-16 17:22:18 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main (String argv[])
+    {
+        Mundo meuMundo = new Mundo();
+
+        meuMundo.desenhaMundo();
+        meuMundo.pausaMundo();
+    }
+}
