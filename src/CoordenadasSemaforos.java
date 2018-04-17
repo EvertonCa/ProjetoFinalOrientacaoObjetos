f002c4b47c0237cf1fc74821b022bb21fdb865d0
EvertonCa <eve.023@hotmail.com>
2018-04-17 01:33:24 -0300

    Adiciona classe Semaforo e CoordenadasSemaforos com as infos do Q1

---
+++ src/CoordenadasSemaforos.java
@@ -0,0 +1,84 @@
+public class CoordenadasSemaforos
+{
+    public int[] getCoordenadasXQ1()
+    {
+        return coordenadasXQ1;
+    }
+
+    public int[] getCoordenadasYQ1()
+    {
+        return coordenadasYQ1;
+    }
+
+    public boolean[] getVerdeQ1()
+    {
+        return verdeQ1;
+    }
+
+    public int[] getCoordenadasXQ2()
+    {
+        return coordenadasXQ2;
+    }
+
+    public int[] getCoordenadasYQ2()
+    {
+        return coordenadasYQ2;
+    }
+
+    public boolean[] getVerdeQ2()
+    {
+        return verdeQ2;
+    }
+
+    public int[] getCoordenadasXQ3()
+    {
+        return coordenadasXQ3;
+    }
+
+    public int[] getCoordenadasYQ3()
+    {
+        return coordenadasYQ3;
+    }
+
+    public boolean[] getVerdeQ3()
+    {
+        return verdeQ3;
+    }
+
+    public int[] getCoordenadasXQ4()
+    {
+        return coordenadasXQ4;
+    }
+
+    public int[] getCoordenadasYQ4()
+    {
+        return coordenadasYQ4;
+    }
+
+    public boolean[] getVerdeQ4()
+    {
+        return verdeQ4;
+    }
+
+    //31 semaforos Q1
+    protected int coordenadasXQ1[] = {3,3,7,7,7,8,8,8,17,17,17,18,18,22,22,22,23,23,32,33,37,37,38,47,47,48,48,52,52,52,53};
+    protected int coordenadasYQ1[] = {15,40,9,34,54,14,39,59,10,35,55,15,40,9,34,54,28,59,55,45,39,59,44,24,40,29,45,23,39,59,44};
+    protected boolean verdeQ1[] = {true, true, true, true, true, false, false, false, true, true, true, true, true,
+            false, false, false, true, true, true, false, true, true, false, false, false, true, true, true, true, true, false};
+
+    //xx semaforos Q2
+    protected int coordenadasXQ2[] = {};
+    protected int coordenadasYQ2[] = {};
+    protected boolean verdeQ2[] = {};
+
+    //xx semaforos Q3
+    protected int coordenadasXQ3[] = {};
+    protected int coordenadasYQ3[] = {};
+    protected boolean verdeQ3[] = {};
+
+    //xx semaforos Q4
+    protected int coordenadasXQ4[] = {};
+    protected int coordenadasYQ4[] = {};
+    protected boolean verdeQ4[] = {};
+
+}
---
+++ src/Semaforo.java
@@ -0,0 +1,12 @@
+public class Semaforo
+{
+    public Semaforo(int x, int y, boolean verde)
+    {
+        this.x = x;
+        this.y = y;
+        this.verde = verde;
+    }
+
+    protected int x, y;
+    protected boolean verde;
+}
