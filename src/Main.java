17b6dde361c0a184b70a299aaa7bac4555462fe6
Lucas P L <lucaslaheras@hotmail.com>
2018-04-18 02:37:28 -0300

    ajuste do semáforo

--- src/Main.java
+++ src/Main.java
@@ -12,11 +12,17 @@ public class Main
 
         ///criação da lista que armazena os objetos semaforos do Q1
         ArrayList <Semaforo> semaforosQ1 = new ArrayList<>();
+        ArrayList <Semaforo> semaforosQ2 = new ArrayList<>();
 
         ///adiciona os objetos semaforos com as coordenadas padrão na lista do Q1
         for (int i=0; i < coordenadas.getSizeQ1(); i++)
         {
             semaforosQ1.add(new Semaforo(coordenadas.getCoordenadasXQ1(i), coordenadas.getCoordenadasYQ1(i), coordenadas.getVerdeQ1(i)));
+        }
+        ///adiciona os objetos semaforos com as coordenadas padrão na lista do Q2
+        for (int i=0; i < coordenadas.getSizeQ2(); i++)
+        {
+            semaforosQ2.add(new Semaforo(coordenadas.getCoordenadasXQ2(i), coordenadas.getCoordenadasYQ2(i), coordenadas.getVerdeQ2(i)));
         }
 
         ///boleano que gerencia se os veiculos entrados pelo usuario chegaram ao destino final
@@ -33,11 +39,15 @@ public class Main
                 {
                     meuMundo.populaSemaforoQ1(semaforosQ1.get(i));
                 }
+                for (int i=0; i < semaforosQ2.size(); i++)
+                {
+                    meuMundo.populaSemaforoQ2(semaforosQ2.get(i));
+                }
 
                 ///desenha o mundo com os quadrantes 1 e 2
                 meuMundo.desenhaMundo(meuMundo.getMundoQ1(), meuMundo.getMundoQ2());
                 ///desenha o mundo com os quadrantes 3 e 4
-                meuMundo.desenhaMundo(meuMundo.getMundoQ3(), meuMundo.getMundoQ4());
+                //meuMundo.desenhaMundo(meuMundo.getMundoQ3(), meuMundo.getMundoQ4());
                 ///pausa a execução do programa pelo tempo determinado
                 meuMundo.pausaMundo();
             }
