580dbab0fc50ab1326282084feb7916a2ce126df
Helmuth <helmuthfilho001@gmail.com>
2018-05-06 23:07:36 -0300

    Ajuste no manipulador dos semaforos

--- src/Semaforo.java
+++ src/Semaforo.java
@@ -9,13 +9,15 @@ public class Semaforo
         this.verde = verde;
         this.duracao = duracao;
         this.contadorDeTempo = 0;
-
+        this.cont = 0;
     }
 
     // Função que faz o semaforo mudar
     public void run(int[][] mapa)
     {
+        //ABAIXO É O TESTE DO LUCAS
         // laço que sempre acontecerá até que o programa finalize
+        /*
         if (this.verde)
         {
             if (this.contadorDeTempo == this.duracao)
@@ -24,21 +26,89 @@ public class Semaforo
             }
             else
             {
-                if ((y + 1 <= 24 && mapa[y+1][x] != 9) && (y - 1 >= 0 && mapa[y-1][x] != 9) && (x + 1 <= 24 && mapa[y][x+1] != 9)
-                        && (x - 1 >= 0 && mapa[y][x-1] != 9))// menor que o tamanho do mapa e não tiver um carro
+                if (y + 1 <= 24) //&& mapa[y+1][x] != 9) && (y - 1 >= 0 && mapa[y-1][x] != 9) && (x + 1 <= 24 && mapa[y][x+1] != 9)
+                        //&& (x - 1 >= 0 && mapa[y][x-1] != 9))// menor que o tamanho do mapa e não tiver um carro
                 {
-                    System.out.println(mapa[y+1][x] + " " + mapa[y-1][x] + " " + mapa[y][x+1] + " " + mapa[y][x-1]);
+                    //System.out.println(mapa[y+1][x] + " " + mapa[y-1][x] + " " + mapa[y][x+1] + " " + mapa[y][x-1]);
+                    //mudarCor();
+
+                    if(mapa[y+1][x] != 9)
+                    {
+                        cont++;
+                    }
+                }
+                else {
+                    cont++;
+                }
+                if(y - 1 >= 0)
+                {
+                    if (mapa[y - 1][x] != 9)
+                    {
+                        cont++;
+                    }
+                }
+                else
+                {
+                    cont++;
+                }
+                if(x + 1 <= 24)
+                {
+                    if(mapa[y][x + 1] != 9)
+                    {
+                        cont++;
+                    }
+                }
+                else
+                {
+                    cont++;
+                }
+                if(x - 1 >= 0)
+                {
+                    if(mapa[y][x - 1] != 9)
+                    {
+                        cont++;
+                    }
+                }
+                else
+                {
+                    cont++;
+                }
+                if(cont == 4)
+                {
                     mudarCor();
                 }
                 else
                 {
                     contadorDeTempo ++;
                 }
+
             }
+
         }
         else
         {
             mudarCor();
+        }
+        */
+        //A PARTIR DAQUI É O TESTE DO HELMUTH
+        while(true)
+        {
+            if(mapa[x + 1][y] == 9)
+            {
+                cont++;
+            }
+            else if(mapa[x - 1][y] == 9)
+            {
+                cont++;
+            }
+            else if(mapa[x][y + 1] == 9)
+            {
+                cont++;
+            }
+            else if(mapa[x][y - 1] == 9)
+            {
+                cont++;
+            }
         }
     }
 
@@ -74,4 +144,5 @@ public class Semaforo
     protected int duracao;
     protected boolean verde;
     protected int contadorDeTempo;
+    protected  int cont;
 }
